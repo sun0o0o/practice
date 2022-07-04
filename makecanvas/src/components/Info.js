80b9f2de5eade970f60266d4381a96a578955965
sun0-1106 <sy.choi1106@gmail.com>
2022-07-04 19:32:16 +0900

    practice

--- makecanvas/src/components/Info.js
+++ makecanvas/src/components/Info.js
@@ -1,45 +1,50 @@
-const getColorKor = color => {
-  console.log('getColorKor');
+import { useMemo } from "react";
+
+const getColorKor = (color) => {
+  console.log("getColorKor");
   switch (color) {
-    case 'red':
-      return '빨강';
-    case 'orange':
-      return '주황';
-    case 'yellow':
-      return '노랑';
-    case 'green':
-      return '초록';
-    case 'blue':
-      return '파랑';
-    case 'navy':
-      return '남';
-    case 'purple':
-      return '보라';
+    case "red":
+      return "빨강";
+    case "orange":
+      return "주황";
+    case "yellow":
+      return "노랑";
+    case "green":
+      return "초록";
+    case "blue":
+      return "파랑";
+    case "navy":
+      return "남";
+    case "purple":
+      return "보라";
     default:
-      return '레인보우';
+      return "레인보우";
   }
 };
 
-const getMovieGenreKor = movie => {
-  console.log('getMovieGenreKor');
+const getMovieGenreKor = (movie) => {
+  console.log("getMovieGenreKor");
   switch (movie) {
-    case 'Marriage Story':
-      return '드라마';
-    case 'The Fast And The Furious':
-      return '액션';
-    case 'Avengers':
-      return '슈퍼히어로';
+    case "Marriage Story":
+      return "드라마";
+    case "The Fast And The Furious":
+      return "액션";
+    case "Avengers":
+      return "슈퍼히어로";
     default:
-      return '아직 잘 모름';
+      return "아직 잘 모름";
   }
 };
 
 const Info = ({ color, movie }) => {
-  const colorKor = getColorKor(color);
-  const movieGenreKor = getMovieGenreKor(movie);
+  //useMemo를 안 쓸 때에는 하나만 변경되도 두 함수가 다 변경됐었다.
+  //useMemo를 쓰면 두 함수가 다 변경되지 않고 []안의 값이 변할 때에만
+  // colorKor,movieGenreKor가 변한다
+  const colorKor = useMemo(() => getColorKor(color), [color]);
+  const movieGenreKor = useMemo(() => getMovieGenreKor(movie), [movie]);
 
   return (
-    <div className='info-wrapper'>
+    <div className="info-wrapper">
       제가 가장 좋아하는 색은 {colorKor} 이고, <br />
       즐겨보는 영화 장르는 {movieGenreKor} 입니다.
     </div>
--- makecanvas/src/pages/UseMemo.js
+++ makecanvas/src/pages/UseMemo.js
@@ -1,53 +1,53 @@
-import Info from '../components/Info';
-import { useState, useCallback } from 'react';
+import Info from "../components/Info";
+import { useState, useCallback } from "react";
 
 const UseMemo = () => {
-  const [color, setColor] = useState('');
-  const [movie, setMovie] = useState('');
+  const [color, setColor] = useState("");
+  const [movie, setMovie] = useState("");
 
-  // const onChangeHandler = e => {
+  const onChangeHandler = (e) => {
+    if (e.target.id === "color") setColor(e.target.value);
+    else setMovie(e.target.value);
+  };
+
+  // const onChangeHandler = useCallback(e => {
   //   if (e.target.id === 'color') setColor(e.target.value);
   //   else setMovie(e.target.value);
-  // };
-
-  const onChangeHandler = useCallback(e => {
-    if (e.target.id === 'color') setColor(e.target.value);
-    else setMovie(e.target.value);
-  }, []);
+  // }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <div>
         <label>
           What is your favorite color of rainbow ?
-          <input id='color' value={color} onChange={onChangeHandler} />
+          <input id="color" value={color} onChange={onChangeHandler} />
         </label>
       </div>
       <div>
         What is your favorite movie among these ?
         <label>
           <input
-            type='radio'
-            name='movie'
-            value='Marriage Story'
+            type="radio"
+            name="movie"
+            value="Marriage Story"
             onChange={onChangeHandler}
           />
           Marriage Story
         </label>
         <label>
           <input
-            type='radio'
-            name='movie'
-            value='The Fast And The Furious'
+            type="radio"
+            name="movie"
+            value="The Fast And The Furious"
             onChange={onChangeHandler}
           />
           The Fast And The Furious
         </label>
         <label>
           <input
-            type='radio'
-            name='movie'
-            value='Avengers'
+            type="radio"
+            name="movie"
+            value="Avengers"
             onChange={onChangeHandler}
           />
           Avengers
